a5a084dfffc0ca3292654f1e777e7c73ec2f2e9b
Camillemille <42758821+Camillemille@users.noreply.github.com>
2018-10-13 12:50:36 -0400

    Commit changes made due to feedback

--- Lab Number 3/Program.cs
+++ Lab Number 3/Program.cs
@@ -16,59 +16,49 @@ namespace Lab_Number_3
                 Console.WriteLine("Enter an integer between 1 and 100"); //Prompts the user to enter a integer.
                 string userInput = Console.ReadLine(); //Declaring a string called userInput whose value is determind by the user.
                 int userNumber = int.Parse(userInput); //Created an integer and called it userNumber. Converted the string into an integer using the parse function. 
-                if (userNumber % 2 == 1) //Checks for odd numbers
-                {
-                    if (userNumber < 60) // Checks that the userNumber is less than 60. 
-                    {
-                        Console.WriteLine("Odd {0}", userNumber);
-                    }
 
+                if (userNumber <= 0 || userNumber > 100)// Tells the program to run if the userNumber is less than or equal to 0 or userNumber is greater than 100.
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100");
                 }
 
-                else
+                else if (userNumber % 2 == 1 && userNumber <= 60) //Gives the program another condition that if the number is odd and less than or equal to 60.
                 {
-                    if (userNumber >= 2 && userNumber < 25) //If the number does not meet the above parameters this runs
-                                                            //If the user number is greater than or equal to two and less than 25 it will run
-                    {
-                        Console.WriteLine("Even and less than 25");
-                    }
+                    Console.WriteLine("Odd {0}", userNumber);
                 }
-                if (userNumber % 4 == 2) // Checks if the userNumber is even.
+
+                else if (userNumber >= 2 && userNumber <= 25 && userNumber % 2 == 0)// Gives the program more specifications to follow.
+                {
+                    Console.WriteLine("Even and less than 25");
+                }
+
+                else if (userNumber >= 26 && userNumber <= 60 && userNumber % 2 == 0)
                 {
 
-                    if (userNumber >= 26 && userNumber < 60) // checks if the userNumber is greater than or equal to 26 and less than 60.
-                    {
-
-                        Console.WriteLine("Even");
-                    }
+                    Console.WriteLine("Even");
                 }
 
 
+                else if (userNumber % 2 == 0 && userNumber > 60) //userNumber has to be even and greater than 60 to run. 
+                {
+                    Console.WriteLine("Even {0}", userNumber);
 
-                if (userNumber % 4 == 2) 
-                {
-                    if (userNumber > 60)
-                    {
-                        Console.WriteLine("Even {0}", userNumber);
-                    }
-                }
-                if (userNumber % 2 == 1)
-                {
-                    if (userNumber > 60)
-                    {
-                        Console.WriteLine("Odd {0}", userNumber);
-                    }
                 }
 
+                else if (userNumber % 2 == 1 && userNumber > 60)
+                {
+                    Console.WriteLine("Odd {0}", userNumber);
+                }
 
+                do //Checking to make sure the user can only put in a y/n.
+                {
+                    Console.WriteLine("Would you like to Continue? (y/n)");
+                    Continue = Console.ReadLine().ToLower(); //Value is assgined before the while loop checks it. 
+                } while (Continue != "n" && Continue != "y");
+            } while (Continue == "y"); 
+                                                           
+            Console.WriteLine("Okay, Bye!"); // End of the program. 
 
-                Console.WriteLine("Would you like to Continue?");
-                Continue = Console.ReadLine().ToLower(); //Value is assgined before the while loop checks it. 
-
-            } while(Continue == "yes" || Continue == "y"); /*This makes it so that even if the user types in an 
-                                                            uppercase "Y" it will still ask to continue.*/ 
-            Console.WriteLine("Okay, Bye!"); // Ends the program. 
-             
         }
     }
 }
